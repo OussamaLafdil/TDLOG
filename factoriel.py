8c7f9fca8b34fca19b54f01f944adb76dfb6a82f
OussamaLafdil <115097468+OussamaLafdil@users.noreply.github.com>
2022-10-05 16:52:48 +0100

    Update factoriel.py

--- factoriel.py
+++ factoriel.py
@@ -1,3 +1,6 @@
-def fact():
-    efaERAERH
-    return 2
+class Weapon:
+    def __init__(self, ammunition: int, range: int,):
+        self._ammunition=ammunition
+        self._range=range
+    def fire_at(self, x: int, y: int, z= int):
+        
