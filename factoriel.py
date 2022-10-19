59f39d0ef763c38102d71f6200f6cc05f0c3df9f
mohamedmamlouk <115096519+mohamedmamlouk@users.noreply.github.com>
2022-10-19 15:33:31 +0100

    Update factoriel.py

--- factoriel.py
+++ factoriel.py
@@ -14,11 +14,14 @@ class Lance_missiles_antisurface(Weapon):
         self._range= 30
 
 
-    def fire_at(self, x: int, y: int):
-        if self._ammunition != 0 :
+    def fire_at(self, x: int, y: int,z : int):
+        if z == 0:
+            if self._ammunition != 0 :
             self._ammunition = self._ammunition-1
-        else :
-            print('NoAmmunitionError')
+            else :
+                print('NoAmmunitionError')
+         else : 
+            prin("OutOfRangeError")
 
 
 class Lance_missiles_antiair(Weapon):
@@ -34,7 +37,7 @@ class Lance_missiles_antiair(Weapon):
             else :
                 print('NoAmmunitionError')
         else:
-            print('error')
+            print('OutOfRangeError')
 
 class Lance_Torpilles(Weapon):
     def __init__(self):
@@ -49,7 +52,7 @@ class Lance_Torpilles(Weapon):
             else :
                 print('NoAmmunitionError')
         else:
-            print('error')
+            print('OutOfRangeError')
 
 arme1=Lance_missiles_antiair()
 arme1.fire_at(1,4,5)
