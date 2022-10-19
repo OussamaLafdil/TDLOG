2940691aace8e0e6fef80f1c40d591edaf9d130d
OussamaLafdil <115097468+OussamaLafdil@users.noreply.github.com>
2022-10-19 15:21:05 +0100

    Update factoriel.py

--- factoriel.py
+++ factoriel.py
@@ -1,6 +1,58 @@
 class Weapon:
-    def __init__(self, ammunition: int, range: int,):
+    def __init__(self, ammunition: int, range: int):
         self._ammunition=ammunition
         self._range=range
-    def fire_at(self, x: int, y: int, z= int):
+    def fire_at(self, x: int, y: int, z: int):
+        if self._ammunition != 0 :
+            self._ammunition = self._ammunition-1
+        else :
+            print('NoAmmunitionError')
+
+class Lance_missiles_antisurface(Weapon):
+    def __init__(self):
+        self._ammunition= 40
+        self._range= 30
+
+
+    def fire_at(self, x: int, y: int):
+        if self._ammunition != 0 :
+            self._ammunition = self._ammunition-1
+        else :
+            print('NoAmmunitionError')
+
+
+class Lance_missiles_antiair(Weapon):
+    def __init__(self):
+        self._ammunition= 50
+        self._range= 40
+
+
+    def fire_at(self, x: int, y: int, z: int):
+        if z>0:
+            if self._ammunition != 0 :
+                self._ammunition = self._ammunition-1
+            else :
+                print('NoAmmunitionError')
+        else:
+            print('error')
+
+class Lance_Torpilles(Weapon):
+    def __init__(self):
+        self._ammunition= 15
+        self._range= 20
+
+
+    def fire_at(self, x: int, y: int, z: int):
+        if z<0:
+            if self._ammunition != 0 :
+                self._ammunition = self._ammunition-1
+            else :
+                print('NoAmmunitionError')
+        else:
+            print('error')
+
+arme1=Lance_missiles_antiair()
+arme1.fire_at(1,4,5)
+arme1.fire_at(1,4,6)
+print(arme1._ammunition)
         
